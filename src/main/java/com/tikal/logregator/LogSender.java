3c706b1ba2f2129f91a1d6850c2245ad892fa025
Haim Cohen <haim.cohen@thetaray.com>
2016-12-07 16:11:33 +0200

    Disable logging to file + change topic name to 2

--- src/main/java/com/tikal/logregator/LogSender.java
+++ src/main/java/com/tikal/logregator/LogSender.java
@@ -19,7 +19,7 @@ import java.util.Properties;
 public class LogSender extends AbstractVerticle{
 
     public static String EVENTBUS_ADDRESS = "eventbus.address";
-    public static String TOPIC = "elblogs";
+    public static String TOPIC = "elblogs2";
 
     public static String KAFKA_IP = "35.158.66.211:9092" ;
 //    public static String KAFKA_IP = "localhost:9092" ;
