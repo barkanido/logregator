5a09b7e52b749bdd580cefdd935d59a22e12d961
yanaif <yanai@tikalk.com>
2016-12-07 12:57:45 +0200

    parsing

---
+++ src/main/java/com/tikal/logregator/LogEntry.java
@@ -0,0 +1,85 @@
+package com.tikal.logregator;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class LogEntry {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(LogEntry.class);
+
+	private static String regex = "([^ ]*) ([^ ]*) ([^ ]*):([0-9]*) (-|[^ :]*):?([0-9]*)? (-?[.0-9]*) (-?[.0-9]*) (-?[.0-9]*) (-|[0-9]*) (-|[0-9]*) ([-0-9]*) ([-0-9]*) \"([^ ]*) ([^ ]*) (- |[^ ]*)\"(?: \"(.*)\" (-|[^ ]*) (-|[^ ]*))?$";
+
+	private static final DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'.'SSSSSS'Z'", Locale.US);
+	private static final Pattern pattern = Pattern.compile(regex);
+
+
+	private long epochTime;
+	private String elbName;
+	private String backendIP;
+	private  float requestProcessingTimeInSec;
+	private  float backendProcessingTimeInSec;
+	private int elbHttpErrorCode;
+	private int backendHttpErrorCode;
+
+	private String httpMethod;
+	private String url;
+
+	@Override
+	public String toString() {
+		return "LogEntry{" +
+				"epochTime=" + epochTime +
+				", elbName='" + elbName + '\'' +
+				", backendIP='" + backendIP + '\'' +
+				", requestProcessingTimeInSec=" + requestProcessingTimeInSec +
+				", backendProcessingTimeInSec=" + backendProcessingTimeInSec +
+				", elbHttpErrorCode=" + elbHttpErrorCode +
+				", backendHttpErrorCode=" + backendHttpErrorCode +
+				", httpMethod='" + httpMethod + '\'' +
+				", url='" + url + '\'' +
+				'}';
+	}
+
+	public LogEntry(long epochTime,
+					String elbName,
+					String backendIP,
+					float requestProcessingTimeInSec,
+					float backendProcessingTimeInSec,
+					int elbHttpErrorCode,
+ 					int backendHttpErrorCode,
+					String httpMethod, String url) {
+		this.epochTime = epochTime;
+		this.elbName = elbName;
+		this.backendIP = backendIP;
+		this.requestProcessingTimeInSec = requestProcessingTimeInSec;
+		this.backendProcessingTimeInSec = backendProcessingTimeInSec;
+		this.elbHttpErrorCode = elbHttpErrorCode;
+		this.backendHttpErrorCode = backendHttpErrorCode;
+		this.httpMethod = httpMethod;
+		this.url = url;
+	}
+
+	public static LogEntry parse(final String line) {
+		final Matcher matcher = pattern.matcher(line);
+		if (!matcher.matches()){
+			logger.error("Bad log entry {}",line);
+			return null;
+		}
+		return new LogEntry(LocalDateTime.parse(matcher.group(1), df).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(),
+				matcher.group(2),
+				matcher.group(5),
+				Float.valueOf(matcher.group(7)),
+				Float.valueOf(matcher.group(8)),
+				Integer.valueOf(matcher.group(10)),
+				Integer.valueOf(matcher.group(11)),
+				matcher.group(14),
+				matcher.group(15)
+		);
+	}
+
+
+}
--- src/main/java/com/tikal/logregator/LogProcessor.java
+++ src/main/java/com/tikal/logregator/LogProcessor.java
@@ -3,6 +3,6 @@ package com.tikal.logregator;
 
 public class LogProcessor {
     public static void main(String[] args) {
-        System.out.println("yanai");
+        System.out.println(LogEntry.parse("2016-12-06T00:25:18.965682Z clicks 173.59.36.238:64740 10.10.22.225:6555 0.000021 0.004194 0.000021 302 302 0 0 \"GET https://app.appsflyer.com:443/id512393983?pid=taptica_int&af_click_lookback=7d&c=iOS_Taptica&tt_cid=79688f751fdb4408a1fa82c45c573613&tt_adv_id=2600&af_siteid=1720&idfa=82D9E6D9-0335-45D6-992E-52F8D6839CB3&sha1_idfa=&mac=&sha1_mac=&af_cpi=4.50000&af_sub1=3730_ HTTP/1.1\" \"Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko) Mobile/14B100\" ECDHE-RSA-AES128-GCM-SHA256 TLSv1.2"));
     }
 }
